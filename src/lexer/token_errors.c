67305189baca5c60d084eb811641fec10ad1c40a
Alexandros Lappas <alappas@c1s5d24.42wolfsburg.de>
2023-11-28 22:20:33 +0100

    Fixed error handling

--- src/lexer/token_errors.c
+++ src/lexer/token_errors.c
@@ -6,7 +6,7 @@
 /*   By: alappas <alappas@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 21:09:35 by vkozlova          #+#    #+#             */
-/*   Updated: 2023/11/11 17:35:46 by alappas          ###   ########.fr       */
+/*   Updated: 2023/11/28 22:19:31 by alappas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 int	check_pipe(t_token *tmp)
 {
-	if (tmp->type == T_PIPE
-		&& (tmp->next->type != T_WORD || !tmp->prev || tmp->prev->type != T_WORD))
+	if (tmp->type == T_PIPE && tmp->next->type != T_WORD && tmp->next->type != T_RED_INP
+		&& tmp->next->type != T_RED_OUT && tmp->next->type != T_APPEND && tmp->next->type != T_DELIM
+		&& !tmp->prev && tmp->prev->type != T_WORD)
 		return (printf("minishell: syntax error near %s\n", \
 		"unexpected token `|'"), 1);
 	return (0);
@@ -28,7 +29,7 @@ int	check_red(t_token *token)
 	{
 		if (token->next->type != T_WORD
 			|| !token->prev || token->prev->type != T_WORD)
-		return (printf("minishell: syntax error near unexpected token `newline'\n"), 1);
+		return (printf("minishell: syntax error near unexpected token `%s'\n", token->next->word), 1);
 	}
 //	if (token->type == T_RED_INP && (token->next->type != T_WORD
 //			|| (token->prev && token->prev->type != T_WORD)))
