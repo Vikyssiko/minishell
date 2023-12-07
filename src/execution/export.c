65633e0042b8cc791e76d8de940509c99dce9bb1
Alexandros Lappas <alappas@c2s17d109.42wolfsburg.de>
2023-12-07 18:51:20 +0100

    Corrected unset/export condition

--- src/execution/export.c
+++ src/execution/export.c
@@ -6,7 +6,7 @@
 /*   By: alappas <alappas@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 21:20:42 by alappas           #+#    #+#             */
-/*   Updated: 2023/12/06 23:26:45 by alappas          ###   ########.fr       */
+/*   Updated: 2023/12/07 18:49:44 by alappas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,7 +106,8 @@ int	export_helper(char *list_arg, t_data *data)
 	while (list_arg[i])
 	{
 		if ((!(ft_isalnum(list_arg[i])) && list_arg[i] != '_'
-				&& list_arg[i] != '=') || (!ft_isalpha(list_arg[0])))
+				&& list_arg[i] != '=') || (!ft_isalpha(list_arg[0])
+				&& list_arg[0] != '_'))
 		{
 			put_to_stderr("minishell: export: `%s': "
 				"not a valid identifier\n", list_arg, data, 1);
